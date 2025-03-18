6e7d6ac10f130b9689dc967c0e9c16da53e11451
Diego Chiodini <diego.chiodini@esmenezes.com.br>
2025-03-18 14:20:39 -0300

    refactor: use auth service

--- app/auth/login.tsx
+++ app/auth/login.tsx
@@ -1,57 +1,43 @@
-import {
-  Button,
-  IconButton,
-  Snackbar,
-  Stack,
-  TextInput,
-} from "@react-native-material/core";
-import {
-  View,
-  Text,
-  StyleSheet,
-  ImageBackground,
-  TouchableOpacity,
-} from "react-native";
+import { Button, IconButton, TextInput } from "@react-native-material/core";
+import { View, Text, StyleSheet, ImageBackground } from "react-native";
 import { MaterialCommunityIcons, Entypo } from "@expo/vector-icons";
 import { useState } from "react";
 import { Link } from "expo-router";
 import useAuth from "../../src/hooks/useAuth";
+import { Stack } from "expo-router";
 
 export default function Login() {
   const { login } = useAuth();
   const [showPassword, setShowPassword] = useState<boolean>(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState(false);
 
-  // const createUserLogin = async () => {
-  //   const user = createUserWithEmailAndPassword(auth, email, pass);
-
-  //   console.log("USER", user);
-  // };
-
-  const handleClick = () => {
-    login(email, password)
-      .then()
-      .catch((err) => console.log("AQUI", err));
+  const handleClick = async () => {
+    await login(email, password);
   };
 
   return (
-    <Stack style={styles.container}>
+    <View style={styles.container}>
+      <Stack.Screen
+        options={{
+          title: "Login",
+        }}
+      />
       <ImageBackground
         source={require("../../src/assets/words.png")}
         resizeMode="cover"
         style={styles.image}
       >
-        <Stack spacing={2} style={styles.areaLogin}>
+        <View style={styles.areaLogin}>
           <Text style={styles.title}>
             Dictionary<Text style={{ color: "#2e2ed2" }}>EN</Text>
           </Text>
 
-          <Stack style={styles.areaInput}>
+          <View style={styles.areaInput}>
             <TextInput
               inputContainerStyle={styles.inputContainer}
               inputStyle={styles.input}
+              placeholder="E-mail"
               trailing={(props) => (
                 <MaterialCommunityIcons
                   name="email"
@@ -83,27 +69,20 @@ export default function Login() {
               value={password}
               onChangeText={(text) => setPassword(text)}
             />
-          </Stack>
-          <Stack style={styles.areaButton}>
+          </View>
+          <View style={styles.areaButton}>
             <Button title="Acessar" onPress={handleClick}></Button>
-          </Stack>
-
-          <Stack>
+          </View>
+          <View>
             <Link href="/auth/signup">
               <Text style={{ color: "#2e2ed2", fontWeight: "bold" }}>
                 Não tem conta? Cadastre-se!
               </Text>
             </Link>
-          </Stack>
-        </Stack>
-        {error && (
-          <Snackbar
-            message="Falha no login."
-            style={{ position: "absolute", start: 16, end: 16, bottom: 16 }}
-          />
-        )}
+          </View>
+        </View>
       </ImageBackground>
-    </Stack>
+    </View>
   );
 }
 
