006c20e12f5ac2a5dfdf3a29890f4b9029617730
Diego Chiodini <diego.chiodini@esmenezes.com.br>
2025-03-18 14:20:56 -0300

    feat: create signup

--- app/auth/signup.tsx
+++ app/auth/signup.tsx
@@ -1,13 +1,89 @@
-import { Link } from "expo-router";
-import { View, Text, StyleSheet } from "react-native";
+import {
+  Button,
+  IconButton,
+  Snackbar,
+  TextInput,
+} from "@react-native-material/core";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  TouchableOpacity,
+  Pressable,
+} from "react-native";
+import { Ionicons, Entypo } from "@expo/vector-icons";
+import { useState } from "react";
+import { Link, router } from "expo-router";
+import useAuth from "../../src/hooks/useAuth";
+import { Stack } from "expo-router";
+import { createUser } from "../../src/services/auth";
 
 export default function Signup() {
+  const { login } = useAuth();
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+
+  const handleClick = async () => {
+    await createUser(email, password);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>TELA SIGNUP</Text>
-      <Link href="/auth/login">
-        <Text>Voltar!</Text>
-      </Link>
+      <Stack.Screen
+        options={{
+          title: "",
+          headerShown: true,
+          headerLeft: () => (
+            <Pressable
+              style={styles.buttonHeader}
+              onPress={() => router.back()}
+            >
+              <Ionicons name="arrow-back" color="#2e2ed2" size={20} />
+              <Text style={styles.textButtonHeader}>Login</Text>
+            </Pressable>
+          ),
+        }}
+      />
+      <View style={styles.areaLogin}>
+        <Text style={styles.title}>Cadastre-se</Text>
+
+        <View style={styles.areaInput}>
+          <TextInput
+            inputContainerStyle={styles.inputContainer}
+            inputStyle={styles.input}
+            placeholder="E-mail"
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+          />
+          <TextInput
+            inputContainerStyle={styles.inputContainer}
+            inputStyle={styles.input}
+            secureTextEntry={!showPassword}
+            placeholder="Senha"
+            trailing={(props) => (
+              <IconButton
+                onPress={() => setShowPassword(!showPassword)}
+                icon={(props) => (
+                  <Entypo
+                    name={showPassword ? "eye-with-line" : "eye"}
+                    {...props}
+                    color="#2e2ed2"
+                  />
+                )}
+                {...props}
+              />
+            )}
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+          />
+        </View>
+        <View style={styles.areaButton}>
+          <Button title="Cadastrar" onPress={handleClick}></Button>
+        </View>
+      </View>
     </View>
   );
 }
@@ -16,6 +92,48 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+  },
+  buttonHeader: {
+    flexDirection: "row",
     alignItems: "center",
+    justifyContent: "center",
+    gap: 8,
+  },
+  textButtonHeader: {
+    color: "#2e2ed2",
+    fontSize: 16,
+    fontWeight: 500,
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  areaLogin: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    padding: 20,
+    paddingBottom: 40,
+    gap: 20,
+  },
+  inputContainer: {
+    backgroundColor: "#FFF",
+  },
+  input: {
+    paddingTop: 10,
+    color: "#2e2ed2",
+  },
+  title: {
+    fontSize: 32,
+    color: "#F00",
+    fontWeight: 600,
+  },
+  areaInput: {
+    width: "90%",
+    gap: 10,
+  },
+  areaButton: {
+    width: "90%",
   },
 });
