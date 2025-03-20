4cbcada21c87ae8b616ad23d31b561a5c5f41ec4
Diego Chiodini <diego.chiodini@esmenezes.com.br>
2025-03-19 23:12:45 -0300

    refactor: wordDetails and save word in history db

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,8 @@
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import axios from "axios";
-import { WordDetails } from "../@types/wordDetails";
+import { WordDetails } from "../@types/word";
+import { words } from "../../words";
+import { getWordHistory, saveWordInHistory } from "./myWords";
 
 const api = axios.create({
   baseURL: "https://api.dictionaryapi.dev/api/v2/",
@@ -9,20 +12,39 @@ export const getWord = async (
   word: string
 ): Promise<WordDetails | undefined> => {
   try {
-    const response = await api.get(`entries/en/${word}`);
+    const response = await api.get(`/entries/en/${word}`);
 
     const details = response.data[0];
 
     const wordDetails = {
-      word: details.word,
-      text: details.phonetics[1].text,
-      audio: details.phonetics[1].audio,
-      synonyms: details.meanings[0].synonyms,
-      meanings: details.meanings[0].definitions[0].definition,
+      word: details?.word,
+      text: details?.phonetics[0]?.text
+        ? details?.phonetics[0]?.text
+        : details?.phonetics[1]?.text,
+      audio: details?.phonetics[0]?.audio
+        ? details?.phonetics[0]?.audio
+        : details?.phonetics[1]?.audio,
+      synonyms: details?.meanings[0]?.synonyms,
+      type: details?.meanings[0]?.partOfSpeech,
+      meanings: details?.meanings?.map((v: any) => ({
+        partOfSpeech: v?.partOfSpeech,
+        definition: v?.definitions[0]?.definition,
+        synonyms: v?.synonyms,
+      })),
     };
 
+    const history = await getWordHistory();
+
+    if (history) {
+      const wordExists = history.includes(word);
+
+      if (!wordExists) {
+        saveWordInHistory(word);
+      }
+    }
+
     return wordDetails;
-  } catch (error) {
-    console.log("Error: ", error);
+  } catch (erro: any) {
+    console.log("Error: ", erro);
   }
 };
