672f732875100b348adbef324b5309b8fa0e8cb3
Diego Chiodini <diego.chiodini@esmenezes.com.br>
2025-03-19 23:08:46 -0300

    refactor: statusbar and Toast position

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,11 +2,11 @@ import { Stack } from "expo-router";
 import { AuthProvider } from "../src/contexts/AuthContext";
 import Toast from "react-native-toast-message";
 import { StatusBar } from "expo-status-bar";
+import { Platform } from "react-native";
 
 export default function RootLayout() {
   return (
     <AuthProvider>
-      <StatusBar style="light" />
       <Stack
         screenOptions={{
           headerShown: false,
@@ -16,7 +16,7 @@ export default function RootLayout() {
         <Stack.Screen name="/auth/login" />
         <Stack.Screen name="/auth/signup" />
       </Stack>
-      <Toast />
+      <Toast topOffset={80} />
       <StatusBar style="auto" />
     </AuthProvider>
   );
